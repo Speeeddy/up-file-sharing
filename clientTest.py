ba070f3557731779e563049403a0dd828c1a528a
Yasif Khan <yasif96@gmail.com>
2018-10-25 15:25:08 +0530

    client test changes

--- clientTest.py
+++ clientTest.py
@@ -11,23 +11,23 @@ class ClientTest(unittest.TestCase):
 	def tearDown(self):
 		x=1
 
-	'''def test_pos_upload(self):
+	def test_pos_a_upload(self):
 		#positive test (upload)
 		upload.setSender("p")
-		upload.setReceiver("q")
-		upload.setFilename("hi.jpg")
+		upload.setReceiver("B")
+		upload.setFilename("t.jpg")
 		req_text = upload.runner()
 		self.assertEqual(req_text,'"File uploaded"\n')
 
-	def test_neg_upload(self):
+	def test_neg_a_upload(self):
 		#negative test (upload)
 		upload.setSender("A1")
-		upload.setReceiver("B1")
+		upload.setReceiver("a2")
 		upload.setFilename("hehe.jpg")
 		req_text = upload.runner()
 		self.assertEqual(req_text,"Invalid")
 
-	def test_pos_store(self):
+	def test_pos_b_store(self):
 		#positive test (file stored in server)
 		resp1 = requests.get(url="http://nipunsood.ooo/fp/B")
 		self.assertNotEqual(resp1.text,'"0"\n')
@@ -36,24 +36,24 @@ class ClientTest(unittest.TestCase):
 		#negative test(file not stored in server)
 		resp1 = requests.get(url="http://nipunsood.ooo/fp/RandomUser")
 		self.assertEqual(resp1.text,'"0"\n')
-'''
+
 	def test_pos_download(self):
-		upload.setSender("r")
-		upload.setReceiver("c")
+		upload.setSender("r1")
+		upload.setReceiver("c1")
 		upload.setFilename("t.jpg")
 		req_text = upload.runner()
 		f1= open("t.jpg","rb")
 		hash1 = hashlib.md5(f1.read()).hexdigest()
 		f1.close()
-		download.setReceiver("c")
+		download.setReceiver("c1")
 		response = download.runner()
 		f2= open("rec_t.jpg","rb")
 		hash2 = hashlib.md5(f2.read()).hexdigest()
 		f2.close()
 		self.assertEqual(hash1,hash2)
 		
-		deleter.setSender("r")
-		deleter.setReceiver("c")
+		deleter.setSender("r1")
+		deleter.setReceiver("c1")
 		deleter.setFilename("t.jpg")
 		response = deleter.runner()
 		self.assertEqual(response,'"File deleted"\n')
---
+++ cmd
@@ -0,0 +1,4 @@
+scp -i up-rest-keypair.pem restflastserver.py ec2-user@13.233.85.191:~
+
+ssh -i up-rest-keypair.pem ec2-user@13.233.85.191
+
